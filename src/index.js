56c1324b171acd8085a07fd910029e62c4d6f691
justgo13 <jgao2299@gmail.com>
2021-11-29 18:31:49 -0500

    initial commit for proejct

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
